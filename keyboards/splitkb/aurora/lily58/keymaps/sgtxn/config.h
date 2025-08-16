859cf999d473921cbd7e8cf997fc23c50ef4da55
sgt <oddobooru@gmail.com>
2025-08-16 20:58:30 +0300

    feat: encoder resolution, debounce type

--- keyboards/splitkb/aurora/lily58/keymaps/sgtxn/config.h
+++ keyboards/splitkb/aurora/lily58/keymaps/sgtxn/config.h
@@ -1,8 +1,10 @@
 #define DEBOUNCE 3
-#define DEBOUNCE_TYPE symm
+#define DEBOUNCE_TYPE sym_defer_pk
 
 #define USB_POLLING_INTERVAL_MS 1
 #define QMK_KEYS_PER_SCAN 12
 
 // Point the generic generated keymap.c to our extras implementation.
 #define OTHER_KEYMAP_C "keyboards/splitkb/aurora/lily58/keymaps/sgtxn/keymap_extras.c"
+
+#define ENCODER_RESOLUTION 2
--- keyboards/splitkb/aurora/lily58/keymaps/sgtxn/keymap_extras.c
+++ keyboards/splitkb/aurora/lily58/keymaps/sgtxn/keymap_extras.c
@@ -1,11 +1,18 @@
 #include QMK_KEYBOARD_H
 
-// Extra behavior layered onto the JSON-generated keymap via OTHER_KEYMAP_C
+// // Extra behavior layered onto the JSON-generated keymap via OTHER_KEYMAP_C
 
-#ifdef ENCODER_ENABLE
-// Both encoders control volume. Returning false stops the kb-level handler.
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    tap_code(clockwise ? KC_VOLU : KC_VOLD);
-    return false;
-}
+// #ifdef ENCODER_ENABLE
+// // Both encoders control volume. Returning false stops the kb-level handler.
+// bool encoder_update_user(uint8_t index, bool clockwise) {
+//     tap_code(clockwise ? KC_VOLU : KC_VOLD);
+//     return false;
+// }
+// #endif
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [1] = { ENCODER_CCW_CW(UG_HUED, UG_HUEU),  ENCODER_CCW_CW(LCTL(KC_MINUS), LCTL(KC_EQUAL))  },
+};
 #endif
