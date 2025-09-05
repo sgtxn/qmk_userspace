b870b3167f3eaa026bb7cb78cbb22bed72e17883
sgtxn <oddobooru@gmail.com>
2025-09-05 16:45:02 +0300

    fix: flash non-aurora lily

---
+++ keyboards/lily58/rev1/keymaps/sgtxn/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 150
+
+#define DEBOUNCE 5
+#define DEBOUNCE_TYPE sym_defer_g
+
+// #define MASTER_RIGHT // uncomment when flashing the right half
+#define USB_POLLING_INTERVAL_MS 1
+#define QMK_KEYS_PER_SCAN 12
